d0ff6a2da7721cc5d755a6f0be1d943b6d2b8e58
Chris <choch@alaska.edu>
2014-04-16 21:13:04 -0800

    Fleshed out test suite

--- shapes_test.cpp
+++ shapes_test.cpp
@@ -1,23 +1,13 @@
 #include <memory>
 using std::make_shared;
 using std::shared_ptr;
-#include <map>
-using std::map;
-#include <utility>
-using std::make_pair;
-using std::pair;
 #include "ShapeClass.h"
 
-map<shared_ptr<Shape>, string> shapes {
-    make_pair<shared_ptr<Shape>, string>(make_shared<Rectangle>(50,40), "rectangle"),
-    make_pair<shared_ptr<Shape>, string>(make_shared<Circle>(50), "circle"),
-    make_pair<shared_ptr<Shape>, string>(make_shared<Spacer>(50, 40), "spacer")
-};
 
-void shape_test(const pair<shared_ptr<Shape>, string> & test_shape)
+void rectangle_test()
 {
-    shared_ptr<Shape> shape = test_shape.first;
-    string shape_name = test_shape.second+".ps";
+    shared_ptr<Shape> shape = make_shared<Rectangle>(50, 40);
+    string shape_name = "rectangle.ps";
 
     shared_ptr<Shape> rotation = make_shared<Rotation>(shape, 90);
     shared_ptr<Shape> scaled = make_shared<Scaled>(shape, 2, 3);
@@ -43,11 +33,69 @@ void shape_test(const pair<shared_ptr<Shape>, string> & test_shape)
     scaled =    make_shared<Scaled>(rotation, 2, 3);
     scaled->draw("rotated_scaled_"+shape_name, 300, 300);
 }
+void circle_test()
+{
+    shared_ptr<Shape> shape = make_shared<Circle>(50);
+    string shape_name = "circle.ps";
+
+    shared_ptr<Shape> rotation = make_shared<Rotation>(shape, 90);
+    shared_ptr<Shape> scaled = make_shared<Scaled>(shape, 2, 3);
+
+    shape = make_shared<Circle>(50);
+    shape->draw(shape_name, 100, 100);
+
+    shape = make_shared<Circle>(50);
+    rotation =  make_shared<Rotation>(shape, 90);
+    rotation->draw("rotated_"+shape_name, 100, 100);
+
+    shape = make_shared<Circle>(50);
+    scaled =    make_shared<Scaled>(shape, 2, 3);
+    scaled->draw("scaled_"+shape_name, 300, 300);
+
+    shape = make_shared<Circle>(50);
+    scaled =    make_shared<Scaled>(shape, 2, 3);
+    rotation =  make_shared<Rotation>(scaled, 90);
+    rotation->draw("scaled_rotated_"+shape_name, 300, 300);
+
+    shape = make_shared<Circle>(50);
+    rotation =  make_shared<Rotation>(shape, 90);
+    scaled =    make_shared<Scaled>(rotation, 2, 3);
+    scaled->draw("rotated_scaled_"+shape_name, 300, 300);
+}
+void spacer_test()
+{
+    shared_ptr<Shape> shape = make_shared<Spacer>(50, 40);
+    string shape_name = "spacer.ps";
+
+    shared_ptr<Shape> rotation = make_shared<Rotation>(shape, 90);
+    shared_ptr<Shape> scaled = make_shared<Scaled>(shape, 2, 3);
+
+    shape = make_shared<Spacer>(50, 40);
+    shape->draw(shape_name, 100, 100);
+
+    shape = make_shared<Spacer>(50, 40);
+    rotation =  make_shared<Rotation>(shape, 90);
+    rotation->draw("rotated_"+shape_name, 100, 100);
+
+    shape = make_shared<Spacer>(50, 40);
+    scaled =    make_shared<Scaled>(shape, 2, 3);
+    scaled->draw("scaled_"+shape_name, 300, 300);
+
+    shape = make_shared<Spacer>(50, 40);
+    scaled =    make_shared<Scaled>(shape, 2, 3);
+    rotation =  make_shared<Rotation>(scaled, 90);
+    rotation->draw("scaled_rotated_"+shape_name, 300, 300);
+
+    shape = make_shared<Spacer>(50, 40);
+    rotation =  make_shared<Rotation>(shape, 90);
+    scaled =    make_shared<Scaled>(rotation, 2, 3);
+    scaled->draw("rotated_scaled_"+shape_name, 300, 300);
+}
 
 int main()
 {
-    for(auto shape : shapes) {
-        shape_test(shape);
-    }
+    rectangle_test();
+    circle_test();
+    spacer_test();
     return 0;
 }
