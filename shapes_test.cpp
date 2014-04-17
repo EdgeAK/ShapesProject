00385b8fddb9bd04097a23a7c69daab23c144777
Chris <choch@alaska.edu>
2014-04-16 19:58:28 -0800

    Compiles with g++, linker errors with gcc and clang. Running g++'s a.out produces some results but fails

--- shapes_test.cpp
+++ shapes_test.cpp
@@ -38,54 +38,54 @@ int main()
 {
     //objects to be tested
     shared_ptr<Shape> rectangle = make_shared<Rectangle>(50, 40);
-    shared_ptr<Shape> circle = make_shared<Circle>(50);
-    shared_ptr<Shape> spacer = make_shared<Spacer>(50, 40);
-    shared_ptr<Shape> rotation = make_shared<Rotation>(rectangle, 90);
-    shared_ptr<Shape> scaled = make_shared<Scaled>(rectangle, 2, 3);
+    shared_ptr<Shape> circle =    make_shared<Circle>(50);
+    shared_ptr<Shape> spacer =    make_shared<Spacer>(50, 40);
+    shared_ptr<Shape> rotation =  make_shared<Rotation>(rectangle, 90);
+    shared_ptr<Shape> scaled =    make_shared<Scaled>(rectangle, 2, 3);
 
     //Tests
-    //Recatngle
+    //Rectangle
     rectangle = make_shared<Rectangle>(50, 40);
     rectangle->draw("rectangle.ps", 100, 100);
 
     rectangle = make_shared<Rectangle>(50, 40);
-    rotation = make_shared<Rotation>(rectangle, 90);
+    rotation =  make_shared<Rotation>(rectangle, 90);
     rotation->draw("rotated_rectangle.ps", 100, 100);
 
     rectangle = make_shared<Rectangle>(50, 40);
-    scaled = make_shared<Scaled>(rectangle, 2, 3);
+    scaled =    make_shared<Scaled>(rectangle, 2, 3);
     scaled->draw("scaled_rectangle.ps", 300, 300);
 
     rectangle = make_shared<Rectangle>(50, 40);
-    scaled = make_shared<Scaled>(rectangle, 2, 3);
-    rotation = make_shared<Rotation>(scaled, 90);
+    scaled =    make_shared<Scaled>(rectangle, 2, 3);
+    rotation =  make_shared<Rotation>(scaled, 90);
     rotation->draw("scaled_rotated_rectangle.ps", 300, 300);
 
     rectangle = make_shared<Rectangle>(50, 40);
-    rotation = make_shared<Rotation>(rectangle, 90);
-    scaled = make_shared<Scaled>(rotation, 2, 3);
+    rotation =  make_shared<Rotation>(rectangle, 90);
+    scaled =    make_shared<Scaled>(rotation, 2, 3);
     scaled->draw("rotated_scaled_rectangle.ps", 300, 300);
 /*
     //Circle
-    circle = make_shared<Circle>(50);
+    circle =   make_shared<Circle>(50);
     circle->draw("circle.ps", 100, 100);
 
-    circle = make_shared<Circle>(50);
+    circle =   make_shared<Circle>(50);
     rotation = make_shared<Rotation>(circle, 90);
     rotation->draw("rotated_circle.ps", 100, 100);
 
-    circle = make_shared<Circle>(50);
-    scaled = make_shared<Scaled>(circle, 2, 3);
+    circle =   make_shared<Circle>(50);
+    scaled =   make_shared<Scaled>(circle, 2, 3);
     scaled->draw("scaled_circle.ps", 300, 300);
 
-    circle = make_shared<Circle>(50);
-    scaled = make_shared<Scaled>(circle, 2, 3);
+    circle =   make_shared<Circle>(50);
+    scaled =   make_shared<Scaled>(circle, 2, 3);
     rotation = make_shared<Rotation>(scaled, 90);
     rotation->draw("scaled_rotated_circle.ps", 300, 300);
 
-    circle = make_shared<Circle>(50);
+    circle =   make_shared<Circle>(50);
     rotation = make_shared<Rotation>(circle, 2, 3);
-    scaled = make_shared<Scaled>(rotation, 90);
+    scaled =   make_shared<Scaled>(rotation, 90);
     scaled->draw("rotated_scaled_circle.ps", 300, 300);
 */
     return 0;
